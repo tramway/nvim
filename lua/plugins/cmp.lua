6fb5eae86992973e627dd4b7f1518aae1e42429a
Marcin Wojtach <marcin.vojtach@gmail.com>
2025-03-23 12:34:02 +0100

    feat: change mapping on cmp plugin

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,13 +3,13 @@ local cmp = require "cmp"
 cmp.setup({
   completion = { completeopt = "menu,menuone,noinsert" },
   mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-j>'] = cmp.mapping.select_prev_item(),
+    ['<C-k>'] = cmp.mapping.select_next_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-u>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<Tab>'] = cmp.mapping.confirm({
+    ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     })
